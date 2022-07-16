f0ea09ceda77be83c330db6ac4799e322d930a40
MuhammadjonR <100756194+MuhammadjonR@users.noreply.github.com>
2022-07-17 01:32:56 +0500

    Update PositiveNumberDetermainer.java

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDetermainer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDetermainer.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDetermainer {
     public void isPositive(int numberToBeDetermined) {
+    if(8>0){
+System.out.print("true");
+}else{
+System.out.print("false");
+}
     }
 }
